794caa786b8a770f20de6de79bcd04ac4c93edc4
JellyMr <Jalexmr02@gmail.com>
2024-10-18 21:55:50 -0600

    JAMM Adicion de migracion para tablas de modulo de ventas

---
+++ AuthAPI903/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthAPI903.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaDeRegistro { get; set; }
+
+        public virtual ICollection<Suscripcion> Suscripciones { get; set; }
+        public virtual ICollection<MetodoPago> MetodosPago { get; set; }
+    }
+}
---
+++ AuthAPI903/Models/Pago.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthAPI903.Models
+{
+    public class Pago
+    {
+        public int Id { get; set; }
+        public decimal Cantidad { get; set; }
+        public DateTime FechaDePago { get; set; }
+
+        public int SuscripcionId { get; set; }
+        public virtual Suscripcion Suscripcion { get; set; }
+    }
+}
