e39eea15262dae7d9c78e44d02bb168cbd0c9efb
JellyMr <Jalexmr02@gmail.com>
2024-11-20 23:35:43 -0600

    Antes de combinar cambios

--- AuthAPI903/obj/Debug/net8.0/AuthAPI903.AssemblyInfo.cs
+++ AuthAPI903/obj/Debug/net8.0/AuthAPI903.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthAPI903")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a309c6758f4ed439b3fa3980172bef4c4650d31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3421a6d2209d6e1974fdc376ee82a6b8e4616e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthAPI903")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthAPI903")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
